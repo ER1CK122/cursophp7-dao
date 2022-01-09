1257d460780e4c92490a62bd15c1a68a60c036b9
ER1CK122 <erckgames53@gmail.com>
2022-01-09 00:50:13 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,17 +2,34 @@
 
 require_once("config.php");
 
-$ari = new Usuario();
 
-$ari->loadByid(3);
+//Carrega um Usuário 
+// $ari = new Usuario();
+// $ari->loadbyid(3);
+//echo $ari;
 
-echo $ari;
+
+//Carrega uma Lista de Usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("ju");
+// echo json_encode($search);
+
+
+//Carrega um usuário usando o login e a senha 
+$usuario = new Usuario();
+$usuario->login("junior", "123456");
+echo $usuario;
+
 
 
 // $sql = new Sql();
-
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 // echo json_encode($usuarios);
+
+
 
  ?>
